4ab59f56f4789cc438acf3a76fda0e4b79945a96
nakamuraos <thinhhoang3030@gmail.com>
2019-10-23 08:14:20 +0700

    Fixed css

---
+++ resources/lang/en/address.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+  'register_title'         => 'Add an address',
+  'register'      => 'Register',
+];
---
+++ resources/lang/vi/address.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+  'register_title'        => 'Thêm địa chỉ mới',
+  'register'              => 'Đăng ký',
+];
